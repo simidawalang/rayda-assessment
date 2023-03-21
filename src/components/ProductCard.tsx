4c5af02ce035f900e6076d4569097e762fb4ac3d
L I L V C <simidawalang@gmail.com>
2023-03-21 15:19:36 +0100

    refactored cards container

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,24 +1,27 @@
-import { MouseEventHandler } from "react";
-import { Box } from "@mui/material";
+import { ReactNode } from "react";
+import { Box, Typography } from "@mui/material";
 import { CustomButton } from "./CustomButton";
 
 type ProductCardProps = {
+  id?: string;
   name?: string;
   title?: string;
   bid?: string;
   image?: string;
-  onClick?: MouseEventHandler;
+  children?: ReactNode;
 };
 
 export const ProductCard = ({
+  id,
   name,
   title,
   bid,
   image,
-  onClick,
+  children,
 }: ProductCardProps) => {
   return (
     <Box
+      key={id}
       sx={{
         border: "1px solid #D0D5DD",
         borderRadius: "12px",
@@ -26,11 +29,66 @@ export const ProductCard = ({
         "& img": {
           width: "100%",
           height: "132px",
+          borderRadius: "12px",
         },
       }}
     >
-      <img src={image} alt={title} />
-      <CustomButton onClick={onClick}>Add to wishlist</CustomButton>
+      <img src={image} alt={name} />
+      <Box sx={{ padding: "0.5rem 0", borderBottom: "1px solid #EAECF0" }}>
+        <Box sx={{ marginBottom: "1rem" }}>
+          <Box
+            sx={{
+              fontSize: "12px",
+              display: "flex",
+              alignItems: "center",
+              "& .name": { fontWeight: "600", marginLeft: "0.5rem" },
+              "& .highest-bidder": {
+                fontWeight: "400",
+                color: "#a5adbe",
+                marginLeft: "0.25rem",
+              },
+            }}
+          >
+            <Box
+              sx={{
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                background: "#F2F4F7",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontWeight: "500",
+                color: "#475467",
+              }}
+            >
+              {name}
+            </Box>
+            <span className="name">{name}</span>{" "}
+            <span className="highest-bidder">&#40;Higest Bidder&#41;</span>
+          </Box>
+        </Box>
+        <Typography
+          sx={{
+            fontSize: "14px",
+            fontWeight: "600",
+            marginBottom: "0.75rem",
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "14px",
+            "& span": { color: "#344054" },
+          }}
+        >
+          <span>Current Bid: </span>
+          {bid}
+        </Typography>
+      </Box>
+
+      <CustomButton sx={{marginTop: "1rem", width: "100%"}}>Add to wishlist</CustomButton>
     </Box>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { loadingSpinner } from "../assets";
-import { CardsContainer, CustomButton } from "../components";
+import { CardsContainer, CustomButton, ProductCard } from "../components";
 
 export const Home = () => {
   const [products, setProducts] = useState([]);
@@ -21,7 +21,7 @@ export const Home = () => {
     } catch (e) {
       console.debug(e);
     }
-    //setLoadingContent(false);
+    setLoadingContent(false);
   };
 
   useEffect(() => {
@@ -29,92 +29,62 @@ export const Home = () => {
   }, []);
   return (
     <Box>
-      Home
-      {loadingContent && (
-        <Box>
-          <img src={loadingSpinner} alt="Loading" />
-          <Typography>Loading...</Typography>
-        </Box>
-      )}
-      <CardsContainer>
-        {products.map(({ name, title, bid, image }, i) => (
-          <Box
-            key={i}
+      <Box sx={{ padding: "1rem" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography sx={{ fontSize: "16px", fontWeight: "600" }}>
+            Featured Items
+          </Typography>
+          <Button
             sx={{
+              color: "#344054",
+              fontSize: "14px",
               border: "1px solid #D0D5DD",
-              borderRadius: "12px",
-              padding: "1rem",
-              "& img": {
-                width: "100%",
-                height: "132px",
-                borderRadius: "12px",
-              },
+              boxShadow: "0px 1px 2px rgba(16, 24, 40, 0.05)",
+              borderRadius: "8px",
+              textTransform: "unset",
             }}
           >
-            <img src={image} alt={name} />
-            <Box
-              sx={{ padding: "0.5rem 0", borderBottom: "1px solid #EAECF0" }}
+            View Auction
+          </Button>
+        </Box>
+      </Box>
+      {loadingContent && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+        >
+          <Box>
+            <img src={loadingSpinner} alt="Loading" />
+            <Typography
+              sx={{ textAlign: "center", fontWeight: "600", fontSize: "2rem" }}
             >
-              <Box sx={{ marginBottom: "1rem" }}>
-                <Box
-                  sx={{
-                    fontSize: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    "& .name": { fontWeight: "600", marginLeft: "0.5rem" },
-                    "& .highest-bidder": {
-                      fontWeight: "400",
-                      color: "#a5adbe",
-                      marginLeft: "0.25rem",
-                    },
-                  }}
-                >
-                  <Box
-                    sx={{
-                      width: "30px",
-                      height: "30px",
-                      borderRadius: "50%",
-                      background: "#F2F4F7",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      fontWeight: "500",
-                      color: "#475467",
-                    }}
-                  >
-                    {name[0]}
-                  </Box>
-                  <span className="name">{name}</span>{" "}
-                  <span className="highest-bidder">
-                    &#40;Higest Bidder&#41;
-                  </span>
-                </Box>
-              </Box>
-              <Typography
-                sx={{
-                  fontSize: "14px",
-                  fontWeight: "600",
-                  marginBottom: "1rem",
-                }}
-              >
-                {title}
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: "14px",
-                  "& span": { color: "#344054" },
-                }}
-              >
-            
-              </Typography>
-            </Box>
-
-            <CustomButton sx={{ width: "100%", marginTop: "1rem" }}>
-              Add to wishlist
-            </CustomButton>
+              Loading...
+            </Typography>
           </Box>
-        ))}
-      </CardsContainer>
+        </Box>
+      )}
+      {!loadingContent && products.length !== 0 && (
+        <CardsContainer>
+          {products.map(({ name, title, bid, image }, i) => (
+            <ProductCard
+              key={i}
+              name={name}
+              title={title}
+              image={image}
+              bid={bid}
+            />
+          ))}
+        </CardsContainer>
+      )}
     </Box>
   );
 };
