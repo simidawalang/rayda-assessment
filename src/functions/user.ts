f29af074d1b0ef4b07526bd1e48e33cb1ab36086
L I L V C <simidawalang@gmail.com>
2023-03-21 18:03:24 +0100

    removed unused folder

--- src/functions/user.ts
+++
@@ -1,8 +0,0 @@
-import { Button, styled } from "@mui/material";
-
-export const MyComponent = styled(Button)({
-  color: "darkslategray",
-  backgroundColor: "aliceblue",
-  padding: 8,
-  borderRadius: 4,
-});
